ec87220284e9734442f79e2c879b6002f319d08e
Wesley Buurke <wesley.buurke@simplicate.nl>
2020-04-17 00:12:34 +0200

    fix: separate Collectible and Dto

--- app/AnimalCrossingIsFun/Dto/Dto.php
+++ app/AnimalCrossingIsFun/Dto/Dto.php
@@ -4,23 +4,7 @@ declare(strict_types=1);
 
 namespace Mave\AnimalCrossingIsFun\Dto;
 
-use Exception;
-
 abstract class Dto {
-
-    protected const IMAGE_PATH = 'random';
-
-    /** @var string */
-    protected $name;
-
-    /** @var string */
-    protected $imageLink;
-
-    /** @var null|string */
-    protected $group;
-
-    /** @var string */
-    protected $safeName;
 
     /**
      * @param array $properties
@@ -29,35 +13,6 @@ abstract class Dto {
         foreach($properties as $property => $value) {
             $this->{$property} = $value;
         }
-    }
-
-    /**
-     * @return string
-     */
-    public function getName(): string {
-        return $this->name;
-    }
-
-    /**
-     * @return string
-     * @throws Exception
-     */
-    public function getImageLink(): string {
-        $path = static::IMAGE_PATH;
-
-        return "/img/{$path}/{$this->getSafeName()}.png";
-    }
-
-    public function getGroup(): ?string {
-        return $this->group;
-    }
-
-    /**
-     * @return string
-     * @throws Exception
-     */
-    public function getSafeName(): string {
-        return str_replace('--', '-', strtolower(preg_replace("/[^\da-z]/i", "-", $this->name)));
     }
 
 }
