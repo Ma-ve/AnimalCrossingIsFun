7380c861528d67ed7b4236ddbf7bcf3995c1813c
Mave <animalcrossing@mave.me>
2020-04-27 15:22:53 +0200

    fix: safeName for events

--- scripts/php-item-safename.php
+++ scripts/php-item-safename.php
@@ -14,17 +14,22 @@ namespace {
     array_shift($arguments);
 
     $allNames = [];
+
     foreach($arguments as $phpFile) {
         $phpFilePath = __DIR__ . '/../' . $phpFile;
         $data = require($phpFilePath);
 
         foreach($data as &$item) {
+            $suffix = '';
             $safeName = Collectible::getSafeNameForString($item['name']);
             if(isset($allNames[$safeName])) {
-                throw new Exception('Safe name ' . $safeName . ' already exists');
+                echo "Safe name '{$safeName}' already exists... ";
+                $suffix = "-" . (string)++$allNames[$safeName];
+                echo ", suffixing it... New name: '{$safeName}{$suffix}'\n";
+            } else {
+                $allNames[$safeName] = 1;
             }
-            $allNames[$safeName] = true;
-            $item['safeName'] = $safeName;
+            $item['safeName'] = $safeName . $suffix;
         }
 
         $varExport = var_export($data, true);
