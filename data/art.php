2fe3f236f7fe001a815053dc6302cd6a13f6a329
Wesley Buurke <wesley.buurke@simplicate.nl>
2020-04-29 20:20:31 +0200

    fix: add safeNames to art

--- data/art.php
+++ data/art.php
@@ -4,257 +4,300 @@
     'name' => 'Academic Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'academic-painting',
   ),
   1 => 
   array (
     'name' => 'Amazing Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'amazing-painting',
   ),
   2 => 
   array (
     'name' => 'Ancient Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'ancient-statue',
   ),
   3 => 
   array (
     'name' => 'Basic Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'basic-painting',
   ),
   4 => 
   array (
     'name' => 'Beautiful Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'beautiful-statue',
   ),
   5 => 
   array (
     'name' => 'Calm Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'calm-painting',
   ),
   6 => 
   array (
     'name' => 'Common Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'common-painting',
   ),
   7 => 
   array (
     'name' => 'Detailed Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'detailed-painting',
   ),
   8 => 
   array (
     'name' => 'Dynamic Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'dynamic-painting',
   ),
   9 => 
   array (
     'name' => 'Familiar Statue',
     'category' => 'Statue',
     'canBeFake' => false,
+    'safeName' => 'familiar-statue',
   ),
   10 => 
   array (
     'name' => 'Famous Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'famous-painting',
   ),
   11 => 
   array (
     'name' => 'Flowery Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'flowery-painting',
   ),
   12 => 
   array (
     'name' => 'Gallant Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'gallant-statue',
   ),
   13 => 
   array (
     'name' => 'Glowing Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'glowing-painting',
   ),
   14 => 
   array (
     'name' => 'Graceful Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'graceful-painting',
   ),
   15 => 
   array (
     'name' => 'Great Statue',
     'category' => 'Statue',
     'canBeFake' => false,
+    'safeName' => 'great-statue',
   ),
   16 => 
   array (
     'name' => 'Informative Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'informative-statue',
   ),
   17 => 
   array (
     'name' => 'Jolly Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'jolly-painting',
   ),
   18 => 
   array (
     'name' => 'Moody Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'moody-painting',
   ),
   19 => 
   array (
     'name' => 'Motherly Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'motherly-statue',
   ),
   20 => 
   array (
     'name' => 'Moving Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'moving-painting',
   ),
   21 => 
   array (
     'name' => 'Mysterious Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'mysterious-painting',
   ),
   22 => 
   array (
     'name' => 'Mystic Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'mystic-statue',
   ),
   23 => 
   array (
     'name' => 'Nice Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'nice-painting',
   ),
   24 => 
   array (
     'name' => 'Perfect Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'perfect-painting',
   ),
   25 => 
   array (
     'name' => 'Proper Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'proper-painting',
   ),
   26 => 
   array (
     'name' => 'Quaint Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'quaint-painting',
   ),
   27 => 
   array (
     'name' => 'Robust Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'robust-statue',
   ),
   28 => 
   array (
     'name' => 'Rock-head Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'rock-head-statue',
   ),
   29 => 
   array (
     'name' => 'Scary Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'scary-painting',
   ),
   30 => 
   array (
     'name' => 'Scenic Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'scenic-painting',
   ),
   31 => 
   array (
     'name' => 'Serene Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'serene-painting',
   ),
   32 => 
   array (
     'name' => 'Sinking Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'sinking-painting',
   ),
   33 => 
   array (
     'name' => 'Solemn Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'solemn-painting',
   ),
   34 => 
   array (
     'name' => 'Tremendous Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'tremendous-statue',
   ),
   35 => 
   array (
     'name' => 'Twinkling Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'twinkling-painting',
   ),
   36 => 
   array (
     'name' => 'Valiant Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'valiant-statue',
   ),
   37 => 
   array (
     'name' => 'Warm Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'warm-painting',
   ),
   38 => 
   array (
     'name' => 'Warrior Statue',
     'category' => 'Statue',
     'canBeFake' => true,
+    'safeName' => 'warrior-statue',
   ),
   39 => 
   array (
     'name' => 'Wild Painting Left Half',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'wild-painting-left-half',
   ),
   40 => 
   array (
     'name' => 'Wild Painting Right Half',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'wild-painting-right-half',
   ),
   41 => 
   array (
     'name' => 'Wistful Painting',
     'category' => 'Painting',
     'canBeFake' => true,
+    'safeName' => 'wistful-painting',
   ),
   42 => 
   array (
     'name' => 'Worthy Painting',
     'category' => 'Painting',
     'canBeFake' => false,
+    'safeName' => 'worthy-painting',
   ),
 );
