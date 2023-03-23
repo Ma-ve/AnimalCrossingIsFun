4d0623bbd96760bed99b27676a66d7e115f9f38e
Mave <mave@local>
2023-03-23 20:17:13 +0100

    fix: deep sea creature months

--- app/AnimalCrossingIsFun/Dto/Collectibles/DeepSeaCreature.php
+++ app/AnimalCrossingIsFun/Dto/Collectibles/DeepSeaCreature.php
@@ -45,6 +45,15 @@ class DeepSeaCreature extends Creature {
                 }
             }
         }
+        $newMonths = [];
+        for($i = 0; $i < 12; $i++) {
+            $newMonths[$i] = in_array($i, $this->months, true);
+            $lowerMonthName = strtolower($this->numToMonth[$i]);
+            if(property_exists($this, $lowerMonthName)) {
+                $this->{$lowerMonthName} = true;
+            }
+        }
+        $this->months = $newMonths;
     }
 
     /**
@@ -58,65 +67,65 @@ class DeepSeaCreature extends Creature {
      * @return int
      */
     public function getShadowSize(): int {
-        return (int)$this->shadowSize;
+        return (int)$this->shadow_size;
     }
 
     public function getSwimmingPattern(): string {
         return $this->swimming_pattern;
     }
 
-    public function hasIrregularMonths() {
-        return true; // @TODO: need to fix weird months => northern = [1, 5] format
-    }
+//    public function hasIrregularMonths() {
+//        return true; // @TODO: need to fix weird months => northern = [1, 5] format
+//    }
 
-    /**
-     * @return string
-     */
-    public function getPeriodOfYear() {
-        $firstMonth = false;
+//    /**
+//     * @return string
+//     */
+//    public function getPeriodOfYear() {
+//        $firstMonth = false;
+//
+//        if(
+//            in_array(1, $this->months, true) &&
+//            in_array(12, $this->months, true)
+//        ) {
+//            for($i = 11; $i >= 0; $i--) {
+//                if(!in_array($i + 1, $this->months, true)) {
+//                    $firstMonth = $i;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        $lastMonth = 0;
+//        for($i = 0; $i < 12; $i++) {
+//            if(in_array($i + 1, $this->months, true)) {
+//                if($firstMonth === false) {
+//                    $firstMonth = $i;
+//                }
+//                $lastMonth = $i;
+//                continue;
+//            }
+//            if(!in_array($i + 1, $this->months, true)) {
+//                if(false !== $firstMonth) {
+//                    break;
+//                }
+//            }
+//        }
+//
+//        return "{$this->numToMonth[$firstMonth]} - {$this->numToMonth[$lastMonth]}";
+//    }
 
-        if(
-            in_array(1, $this->months, true) &&
-            in_array(12, $this->months, true)
-        ) {
-            for($i = 11; $i >= 0; $i--) {
-                if(!in_array($i + 1, $this->months, true)) {
-                    $firstMonth = $i;
-                    break;
-                }
-            }
-        }
-
-        $lastMonth = 0;
-        for($i = 0; $i < 12; $i++) {
-            if(in_array($i + 1, $this->months, true)) {
-                if($firstMonth === false) {
-                    $firstMonth = $i;
-                }
-                $lastMonth = $i;
-                continue;
-            }
-            if(!in_array($i + 1, $this->months, true)) {
-                if(false !== $firstMonth) {
-                    break;
-                }
-            }
-        }
-
-        return "{$this->numToMonth[$firstMonth]} - {$this->numToMonth[$lastMonth]}";
-    }
-
-    public function getBullets(): array {
-        $return = [];
-        for($i = 0; $i <= 11; $i++) {
-            $monthLetter = $this->numToMonth[$i];
-            $return[] = [
-                'month' => $monthLetter,
-                'value' => in_array($i, $this->months, true),
-            ];
-        }
-
-        return $return;
-    }
+//    public function getBullets(): array {
+//        $return = [];
+//        for($i = 0; $i <= 11; $i++) {
+//            $monthLetter = $this->numToMonth[$i];
+//            $return[] = [
+//                'month' => $monthLetter,
+//                'value' => in_array($i, $this->months, true),
+//            ];
+//        }
+//
+//        return $return;
+//    }
 
 }
